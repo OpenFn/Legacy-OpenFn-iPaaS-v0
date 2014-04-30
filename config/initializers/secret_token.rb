c4cfaef639fd79e889840bb87f2e28961c63cf1c
Jonathan Salis <jtsseneca@hotmail.com>
2014-04-30 11:38:52 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SalesForce::Application.config.secret_key_base = 'd85288d0e2144a4d9c8b18bfecdf00199d11e9485db27ff4f6123d033d1c17300bf83ce4c8bd3a29cf608521fb68da85d3c8f7cc84b1138dacdf8d41e6090b28'
