cfd063619e0f3ae55e2183b703ac6628900b9201
Jonathan Salis <jtsseneca@hotmail.com>
2014-05-23 14:57:20 -0400

    Adding filtering to fields

--- app/controllers/odk_form_fields_controller.rb
+++ app/controllers/odk_form_fields_controller.rb
@@ -1,7 +1,10 @@
 class OdkFormFieldsController < ApplicationController
   def index
     odk_form_fields = OdkAggregate::Form.find(params[:odk_form_id]).fields.
-      collect{|f| {field_name: f["nodeset"], field_type: f["type"]}}
+      collect do |f|
+        index = f["nodeset"].index("/", 1)
+        {field_name: f["nodeset"][index..-1], field_type: f["type"]}
+      end
 
     render json: odk_form_fields, root: false
   end
--- config/initializers/odk_aggregate.rb
+++ config/initializers/odk_aggregate.rb
@@ -1,3 +1,4 @@
 OdkAggregate.configure do |config|
   config.version = '1.0'
+  config.base_url = "https://kgvk-odk-sri.appspot.com"
 end
