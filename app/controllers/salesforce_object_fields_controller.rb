e362d564c400ba9da83d793b5e645ca692e240d7
Jonathan Salis <jtsseneca@hotmail.com>
2014-05-26 10:26:40 -0400

    correcting SF field lookup with spaces

--- app/controllers/salesforce_object_fields_controller.rb
+++ app/controllers/salesforce_object_fields_controller.rb
@@ -3,7 +3,7 @@ class SalesforceObjectFieldsController < ApplicationController
   def index
     sf_client = Restforce.new
     sf_fields = sf_client.
-      describe("#{params[:salesforce_object_id]}__c")["fields"].
+      describe("#{params[:salesforce_object_id].gsub(" ", "_")}__c")["fields"].
       select{|f| f["updateable"]}.
       collect{|f| {object_name: params[:salesforce_object_id], field_name: f["name"], odk_fields: []}}
 
