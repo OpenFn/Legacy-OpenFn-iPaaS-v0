3b091addb318f66cebcf25f1cac64703ead02a62
Jonathan Salis <jtsseneca@hotmail.com>
2014-05-30 12:03:45 -0400

    Changing minification in production again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ SalesForce::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(:mangle => false) }
+  config.assets.js_compressor = Uglifier.new(mangle: false)
 end
