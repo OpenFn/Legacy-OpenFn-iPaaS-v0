2f94c28dbfd7ec45167565848b2e867faf0c1d7a
Jonathan Salis <jtsseneca@hotmail.com>
2014-06-10 22:50:59 -0400

    Cleaning up unused code

--- app/workers/odk_to_salesforce/runner.rb
+++ app/workers/odk_to_salesforce/runner.rb
@@ -48,63 +48,13 @@ module OdkToSalesforce
         # => Now we're at the top object!
         # => Find or create it
         puts "-> find or create #{node[:name]}"
-        #thing = find_or_create_one_or_many(node[:name], constraints)
-        #return thing
 
         # => Return the target object
         return current_object
       end
-
-      # => One of the parents didn't create properly!
-      # if parent_objects.include?(false)
-      #   # => Return false back up the chain
-      #   return false
-      # else
-      #   parent_objects.flatten.each do |parent_object|
-      #     # => Load settings from the parent object that was just created
-      #     parent_type = parent_object["attributes"]["type"]
-      #     parent_type_field = node[:parents][parent_type.to_sym]
-
-      #     # => Set the child's parent object with the found or created parent object id
-      #     constraints[parent_type_field.to_sym] = parent_object["Id"]
-      #   end
-      #   # => Now create this object and pass it along
-      #   return find_or_create_one_or_many(node[:name], constraints)
-      # end
     end
 
     private
-
-    ##
-    # If any of the fields is an array
-    #
-    #  { field: [value1, value2] }
-    #
-    # then we must assume they all are. In this case,
-    # we iterate over the first array and create a flat hash of
-    # constraints which we then pass to find_or_create on each iterations.
-
-    # => TODO: When a repeated_field is pointing to the object
-    def find_or_create_one_or_many(object_name, constraints)
-      if constraints.flatten.any? { |e| e.kind_of?(Array) }
-        success_array = []
-        constraints.flatten.select { |e| e.kind_of?(Array)}[0].each_with_index do |c, i|
-          flat_constraints = {}
-          constraints.each do |k,v|
-            # only iterate on arrays, otherwise make value the same for all
-            if v.kind_of?(Array)
-              flat_constraints[k] = v[i]
-            else
-              flat_constraints[k] = v
-            end
-          end
-          success_array << find_or_create(object_name, flat_constraints)
-        end
-        return success_array
-      else
-        return find_or_create(object_name, constraints)
-      end
-    end
 
     ##
     # Checks if to go up the tree or continue creating current element
@@ -124,96 +74,6 @@ module OdkToSalesforce
       end
 
       has_parents && mapping_includes_parents
-    end
-
-    def find_or_create(object_name, constraints)
-      #raise constraints.inspect
-      sf_object = nil
-      #sf_object = find(object_name, constraints)
-      #puts "-> found #{object_name}, #{sf_object["Id"]}".magenta if !sf_object.nil?
-
-      if sf_object.nil?
-        puts "-> no #{object_name} fitting constraints"
-        # begin
-          sf_id = create(object_name, constraints)
-          sf_object = find(object_name, { Id: sf_id })
-          sf_object = false if sf_object.nil?
-        # rescue
-        #   sf_object = false
-        # end
-      end
-
-      sf_object
-    end
-
-    def find(object_name, constraints)
-      and_or_or = "AND"
-      # if applicable, we want only to search by unique identifier fields
-      has_uniques = false
-      unique_constraints = only_unique_fields_if_any(object_name, constraints)
-
-      if unique_constraints[0]
-        has_uniques = true
-        and_or_or = "OR"
-        constraints = unique_constraints[1]
-      end
-
-      begin
-        query_string = "SELECT Id FROM #{object_name} WHERE "
-        constraints.each do |k, v|
-          quote = ""
-
-          if v.kind_of?(String)
-            quote = "'"
-          end
-
-          if valid_for_query?(k)
-            # => Set the lookup value so empty strings can be compared too
-            lookup = "#{quote}#{v}#{quote}"
-            query_string += "#{k} = #{lookup} #{and_or_or} " unless lookup.blank?
-          end
-        end
-        # remove trailing space and AND
-        query_string = query_string[0..-6] if and_or_or == "AND"
-        query_string = query_string[0..-5] if and_or_or == "OR"
-
-        @rf.query(query_string)["records"].first
-      rescue Exception => e
-        puts "-> something wrong in Salesforce query:".red
-        puts e.message
-        return nil
-      end
-    end
-
-    def valid_for_query?(key)
-      ![:Repayment_Problem_Explanation__c].include?(key)
-    end
-
-    ##
-    # If any of the field given are unique identifiers, return a hash of only
-    # those fields.
-    def only_unique_fields_if_any(object_name, constraints)
-      uniques = {}
-      has_uniques = false
-      constraints.each do |k, v|
-        if @relationships[object_name.to_sym][:uniques].include?(k.to_s)
-          has_uniques = true
-          uniques[k] = v
-        end
-      end
-
-      uniques = constraints if uniques.empty?
-      return [has_uniques, uniques]
-    end
-
-    def create(object_name, constraints)
-      response = @rf.create!(object_name, constraints)
-      if !response
-        puts "-> Failed to create #{object_name}, on #{constraints}".red if !response
-      else
-        puts "-> created new #{object_name}, #{response}".green.bold
-      end
-      response
     end
   end
 end
