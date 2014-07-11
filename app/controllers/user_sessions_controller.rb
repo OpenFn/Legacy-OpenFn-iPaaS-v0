9cb746d553b22d167efd19d3c912622bacd29540
Jonathan Salis <jtsseneca@hotmail.com>
2014-07-10 23:44:36 -0400

    Updating login and odk_url

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,12 +1,8 @@
 class UserSessionsController < ApplicationController
   skip_before_filter :require_login, except: [:destroy]
 
-  def new
-    @user = User.new
-  end
-
   def create
-    if @user = login(params[:email], params[:password])
+    if login(params[:email], params[:password])
       redirect_to(:root, notice: "Welcome Back!")
     else
       flash.now[:alert] = 'Login failed'
