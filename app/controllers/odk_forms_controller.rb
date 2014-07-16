e47bed3c659684cf39c99633c7c7ceda570dc24b
noam <noam@clusterfoo.com>
2014-07-16 19:22:11 -0400

    card 36 -- fix odk stuff

--- app/controllers/odk_forms_controller.rb
+++ app/controllers/odk_forms_controller.rb
@@ -1,7 +1,9 @@
 class OdkFormsController < ApplicationController
 
   def index
-    odk = OdkAggregate::Connection.new(current_user.odk_url)
+    odk = OdkAggregate::Connection.new(current_user.odk_url,
+                                       current_user.odk_username,
+                                       current_user.odk_password)
     odk_forms = odk.all_forms.collect(&:form_id).sort
     render json: odk_forms
   end
