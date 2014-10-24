4eabecd7c06eedc4f024ab71cdf3784b183b77c8
Nic Young <nic.youngster@gmail.com>
2014-10-24 09:30:19 +0200

    Added flat-ui bootstrap theme.

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module SalesForce
     # config.i18n.default_locale = :de
 
     config.assets.paths << "#{Rails.root}/app/assets/templates"
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
