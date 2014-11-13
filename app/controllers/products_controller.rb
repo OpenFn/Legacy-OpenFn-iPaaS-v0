8f8f223fc14e67962ad37adbd2e87999a89babc2
Nic Young <nic.youngster@gmail.com>
2014-11-13 16:54:11 +0200

    not requiring login

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   respond_to :json
   layout false
 
-  skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token, :require_login
 
   def index
     render json: Product.order('name').as_json
