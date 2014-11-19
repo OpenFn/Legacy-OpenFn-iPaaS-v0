9e201c54e8f84705a2597b96d6e566e15909e7ed
Stuart Corbishley <corbish@gmail.com>
2014-11-19 16:35:55 +0200

    Treat 'yes', 'no' strings as boolean. #54

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env)
 
 module SalesForce
   class Application < Rails::Application
+
+    require 'to_bool'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
