1092dc3b151cdbae99a2ba2d35e4581b24235be9
Nic Young <nic.youngster@gmail.com>
2014-11-20 13:18:55 +0200

    Added costs, reviews, resources, and provider to products

--- app/models/salesforce/listing/product.rb
+++ app/models/salesforce/listing/product.rb
@@ -1,5 +1,5 @@
 class Salesforce::Listing::Product
-  attr_reader :id, :description, :name, :tags, :website, :enabled, :integrated
+  attr_reader :id, :description, :name, :tags, :website, :enabled, :integrated, :costs, :reviews, :resources, :provider
 
   def initialize(notification)
     raise ArgumentError, "Invalid notification: #{notification.class}" unless notification.is_a?(Salesforce::Notification)
@@ -11,5 +11,9 @@ class Salesforce::Listing::Product
     @name = notification.at_css('Name').try(:content)
     @tags = notification.at_css('Tags__c').try(:content)
     @website = notification.at_css('Website__c').try(:content)
+    @costs = notification.at_css('Costs__c').try(:content)
+    @reviews = notification.at_css('Reviews__c').try(:content)
+    @resources = notification.at_css('Resources__c').try(:content)
+    @provider = notification.at_css('Provider__c').try(:content)
   end
 end
