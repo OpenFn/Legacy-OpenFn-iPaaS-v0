9eee97cebc55c28d9b1a34667c438f6ccd775c0a
Stuart Corbishley <corbish@gmail.com>
2014-11-24 17:06:54 +0200

    Fix ODK Cursors. #56
    The last_uuid wasn't getting persisted, since it can't be changed
    from the UI, and the cursor is saved in it's entirety the use of the
    UUID column on the Import table was removed.
    
    Next up: remove the last_uuid column to avoid ambiguity.

--- app/workers/odk_to_salesforce/odk.rb
+++ app/workers/odk_to_salesforce/odk.rb
@@ -5,12 +5,12 @@ module OdkToSalesforce
   class Odk
     attr_reader :submissions, :import
 
-    def initialize(form_id, import, limit, user)
+    def initialize(form_name, import, limit, user)
 
       @connection = OdkAggregate::Connection.new(user["odk_url"], user["odk_username"], user["odk_password"])
 
-      # { id: "form_id", topElement: "form_top_element"}
-      @form = fetch_form(form_id)
+      # { id: "form_name", topElement: "form_top_element"}
+      @form = fetch_form(form_name)
 
       @import = import
       @limit = limit
@@ -22,7 +22,7 @@ module OdkToSalesforce
     # [ id, id, id ... ]
     def fetch_submissions
       params = {formId: @form[:id], numEntries: @import.num_imported? ? @limit + @import.num_imported : @limit}
-      params.merge!(cursor: transform_cursor(@import.cursor)) if @import.cursor && @import.last_uuid
+      params.merge!(cursor: transform_cursor(@import.cursor)) if @import.cursor
       @request = @connection.submissions_where(params)
 
       import.update(cursor: @request.opaque_data)
@@ -42,22 +42,26 @@ module OdkToSalesforce
 
     private
 
-    def fetch_form(form_id)
+    def fetch_form(form_name)
       form = {
-        id: form_id,
-        topElement: @connection.find_form(form_id).get_top_element
+        id: form_name,
+        topElement: @connection.find_form(form_name).get_top_element
       }
     end
 
 
     def transform_cursor(cursor)
+      # <cursor xmlns=\"http://www.opendatakit.org/cursor\">
+      #   <attributeName>_LAST_UPDATE_DATE</attributeName>
+      #   <attributeValue>2014-10-21T05:02:58.000+0000</attributeValue>
+      #   <uriLastReturnedValue>uuid:7b018f38-ba6e-48f9-99ef-5f4d6c8ad6ba</uriLastReturnedValue>
+      #   <isForwardCursor>true</isForwardCursor>
+      # </cursor>
+
       # => Reset the day to some time far in the past
       # => This allows the UUID lookup to have a lot more weight
       cursor.match(/<attributeValue>(\S+)<\/attributeValue>/) do
         cursor.gsub!($1, "2000-01-01T00:00:00.000+0000")
-      end
-      cursor.match(/<uriLastReturnedValue>(\S+)<\/uriLastReturnedValue>/) do
-        cursor.gsub!($1, @import.last_uuid)
       end
     end
   end
