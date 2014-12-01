722d16cab587ed62d42d9dd7498eeea7a991fc7f
Nic Young <nic.youngster@gmail.com>
2014-12-01 17:19:08 +0200

    Implemented and spec'd the integration pipeline

--- app/models/submission/dispatch.rb
+++ app/models/submission/dispatch.rb
@@ -1,5 +1,7 @@
 #designsketch
 
+# only for PUSH destination integrations. Pulls can be handled by storing destination payloads for collection.
+
 class Submission::Dispatch
   
   def initialize(submission)
