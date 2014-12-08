44acf52b9cdb6221577a8993f32ee0e167f602dc
Stuart Corbishley <corbish@gmail.com>
2014-12-08 09:50:06 +0200

    OdkForm class now using the new OdkClient for retrieving form information

--- app/models/odk_form.rb
+++ app/models/odk_form.rb
@@ -1,3 +1,4 @@
+require 'odk_client'
 class OdkForm < ActiveRecord::Base
   belongs_to :mapping
 
@@ -11,23 +12,16 @@ class OdkForm < ActiveRecord::Base
 
   def populate_fields
     current_user = mapping.user
-    odk = OdkAggregate::Connection.new(current_user.odk_url, current_user.odk_username, current_user.odk_password)
-    odk_form_fields = odk.find_form(self.name).fields.collect do |f|
-      index = f["nodeset"].index("/", 1)
-      {
-        field_name: f["nodeset"][index..-1],
-        field_type: f["type"],
-        group: f["group"]
-      }
+    client = OdkClient.new( current_user.odk_url, username: current_user.odk_username, password: current_user.odk_password )
+
+    client.get_form(self.name).parse.sort_by_grouping.each do |field|
+      odk_fields.build({
+        field_name: field[:path],
+        field_type: field[:type],
+        repeat_field: !!field[:repeat],
+        uuidable: field[:path] == "/meta/instanceID"
+      })
     end
 
-    odk_form_fields.each do |odk_form_field|
-      self.odk_fields.build({
-        field_name: odk_form_field[:field_name],
-        field_type: odk_form_field[:field_type],
-        repeat_field: odk_form_field[:group],
-        uuidable: odk_form_field[:field_name] == "/meta/instanceID"
-      }) unless self.odk_fields.detect{|field| field.field_name.eql?(odk_form_field[:field_name])}
-    end
   end
 end
