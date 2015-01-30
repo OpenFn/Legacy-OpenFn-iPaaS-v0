8e5b7b978f52ba63d06d67a848c3ecd197b9d111
Stuart Corbishley <corbish@gmail.com>
2015-01-30 15:13:58 +0200

    Use referenceTo when creating linked objects in SF

--- db/migrate/20150123112011_add_field_attributes_to_salesforce_fields.rb
+++ db/migrate/20150123112011_add_field_attributes_to_salesforce_fields.rb
@@ -2,29 +2,28 @@ class AddFieldAttributesToSalesforceFields < ActiveRecord::Migration
   def up
     add_column :salesforce_fields, :properties, :json
 
-    SalesforceField.all.each do |field|
+    SalesforceField.transaction do
+      SalesforceField.unscoped.all.each do |field|
 
-      # Skip records that have been imported after this.
-      if field.data_type
+        # Skip records that have been imported after this.
+        if field.data_type
+          begin
+            properties = field.properties || {}
 
-        unless field.properties
-          field.properties = {}
+            field.properties = properties.merge({
+              "name" => field.field_name,
+              "type" => field.data_type.camelize(:lower),
+              "referenceTo" => field.reference_to ? [field.reference_to] : nil,
+              "label" => field.label_name,
+              "nillable" => field.nillable,
+              "unique" => field.unique
+            })
+            field.save!
+          rescue => e
+            puts field.inspect
+            raise e
+          end
         end
-
-        begin
-          field.properties.merge({
-            type: field.data_type.camelize(:lower),
-            referenceTo: field.reference_to ? [field.reference_to] : nil,
-            label: field.label_name,
-            nillable: field.nillable,
-            unique: field.unique
-          })
-        rescue => e
-          puts field.inspect
-          raise e
-        end
-
-        field.save!
       end
     end
   end
