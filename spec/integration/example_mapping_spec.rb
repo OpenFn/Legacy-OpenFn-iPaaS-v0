c8b6722861928d5746176e7c176bf622492da005
Stuart Corbishley <corbish@gmail.com>
2015-02-02 13:10:39 +0200

    Updated README and tidyup

--- spec/integration/example_mapping_spec.rb
+++ spec/integration/example_mapping_spec.rb
@@ -5,22 +5,19 @@ RSpec.describe "Example Mapping Submission Integration", :type => :integration d
   # The mapping examples are namespaced, rspec can't infer the class/table names
   # from the namespaced fixures.
   def self.mapping_fixture(name)
-    fixtures "#{name}/mappings"
-    set_fixture_class "#{name}/mappings" => Mapping
-    fixtures "#{name}/submissions"
-    set_fixture_class "#{name}/submissions" => Submission
-    fixtures "#{name}/users"
-    set_fixture_class "#{name}/users" => User
-    fixtures "#{name}/salesforce_objects"
-    set_fixture_class "#{name}/salesforce_objects" => SalesforceObject
-    fixtures "#{name}/salesforce_fields"
-    set_fixture_class "#{name}/salesforce_fields" => SalesforceField
-    fixtures "#{name}/salesforce_relationships"
-    set_fixture_class "#{name}/salesforce_relationships" => SalesforceRelationship
-    fixtures "#{name}/odk_fields"
-    set_fixture_class "#{name}/odk_fields" => OdkField
-    fixtures "#{name}/odk_field_salesforce_fields"
-    set_fixture_class "#{name}/odk_field_salesforce_fields" => OdkFieldSalesforceField
+    [
+      Mapping,
+      Submission,
+      User,
+      SalesforceObject,
+      SalesforceField,
+      SalesforceRelationship,
+      OdkField,
+      OdkFieldSalesforceField
+    ].each { |klass|
+      fixtures "#{name}/#{klass.table_name}"
+      set_fixture_class "#{name}/#{klass.table_name}" => klass
+    }
   end
 
   mapping_fixture('complex_survey')
