2875670783b95c97d9f965eb6d285293ae282415
taylor <downs.taylor@gmail.com>
2015-02-17 17:16:29 +0200

    add plan reference to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217143344) do
+ActiveRecord::Schema.define(version: 20150217150221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -219,9 +219,11 @@ ActiveRecord::Schema.define(version: 20150217143344) do
     t.string   "last_name"
     t.string   "organisation"
     t.string   "tier"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["plan_id"], name: "index_users_on_plan_id", using: :btree
 
   create_table "votes", force: true do |t|
     t.integer "user_id"
