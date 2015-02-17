948cfe2fcd98ac8e29101db6236cce571c47d91c
taylor <downs.taylor@gmail.com>
2015-02-17 10:17:54 +0200

    skip before filter

--- app/controllers/welcome_stats_controller.rb
+++ app/controllers/welcome_stats_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeStatsController < ApplicationController
 
+skip_before_filter :require_login
+
   def show
     render json: {
-      #code submissionCount + 35,605 for KGVK's pre-reset submissions
+      #We add 35,605 to submissionCount for KGVK's pre-reset submissions
       submissionCount: Submission.where(state: 'success').count + 35605,
       orgCount: User.where(role: 'client').count,
       productPublicCount: Product.where(enabled: true).count,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217062350) do
+ActiveRecord::Schema.define(version: 20150217080204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -205,7 +205,6 @@ ActiveRecord::Schema.define(version: 20150217062350) do
     t.string   "last_name"
     t.string   "organisation"
     t.string   "tier"
-    t.string   "sf_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
