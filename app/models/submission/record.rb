4fe83472d320ab6e3053fe1e5c4f0a05274473e7
Stuart Corbishley <corbish@gmail.com>
2015-02-20 13:10:45 +0200

    Submission Records belong to Mapping

--- app/models/submission/record.rb
+++ app/models/submission/record.rb
@@ -1,7 +1,8 @@
-#designsketch
-
 class Submission::Record < ActiveRecord::Base
-  belongs_to :integration
+  self.table_name = "submission_records"
+  belongs_to :mapping
+
+  validates_presence_of :mapping, :raw_source_payload
 
   def submitted!
     self.processed_at = Time.now
--- spec/models/submission/record_spec.rb
+++ spec/models/submission/record_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Submission::Record, :type => :model do
 
-  it "should be tested" do
-    raise NotImplementedError
-  end
+  it { should belong_to :mapping }
   
+  it { should validate_presence_of :mapping }
+  it { should validate_presence_of :raw_source_payload }
+
 end
