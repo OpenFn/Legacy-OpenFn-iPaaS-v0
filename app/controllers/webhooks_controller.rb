23cdb3b07a331cbc674fc3508a71352cebe1d2ae
Ankur jain <ankur.jain@ongraph.com>
2015-03-03 18:33:51 +0530

    charge success webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -10,17 +10,35 @@ class WebhooksController < ApplicationController
 		event_type = data_json['type']
 
 		case event_type
+		when "charge.succeeded"
+			update_subscription_period data_json
+
 		when "charge.failed"
       charge_failed data_json
+
 		end
 		render nothing: true
 	end
 
+	def update_subscription_period data_json
+		customer_id =  data_json['data']['object']['customer']
+		begin
+			customer = Stripe::Customer.retrieve(customer_id)
+			span = customer.subscriptions.data[0].plan.interval
+			if customer.present?
+				user = User.find_by_email(customer.email)
+				if user.present?
+					user.stripe_curent_period_end = Time.at(customer.subscriptions.data[0].current_period_end)
+					user.save
+				end
+			end
+		rescue
+			return
+		end
+	end
+
 	def charge_failed data_json
 		customer_id =  data_json['data']['object']['customer']
-		charge_id = data_json['data']['object']['id']
-		amount_pence = data_json['data']['object']['amount']
-		amount = (amount_pence.to_f / 100)
 		begin
 			customer = Stripe::Customer.retrieve(customer_id)
 			if customer.present?
