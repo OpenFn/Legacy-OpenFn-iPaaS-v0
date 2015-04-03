3f68b4c7de36b8cf8a0fc32246ac7429e04a3033
Ile Eftimov <ileeftimov@gmail.com>
2015-04-03 16:20:47 +0200

    wip

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,29 +1,47 @@
 class ChargesController < ApplicationController
 
-skip_before_filter :require_login
+	skip_before_filter :require_login
 
 	def new
 	end
 
 	def create
+		puts '@@@@@@@@@@@@@'
+		puts '@@@@@@@@@@@@@'
+		puts params
+		puts '@@@@@@@@@@@@@'
+		puts '@@@@@@@@@@@@@'
+
 	  # Amount in cents
-	  @amount = 500
+	  @amount = charges_params[:amount]
 
 	  customer = Stripe::Customer.create(
-	    :email => 'example@stripe.com',
-	    :card  => params[:stripeToken]
-	  )
+	  	:email => 'example@stripe.com',
+	  	:card  => charges_params[:token]
+	  	)
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => @amount,
-	    :description => 'Rails Stripe customer',
-	    :currency    => 'usd'
-	  )
+	  begin
+		  charge = Stripe::Charge.create(
+		  	:customer    => customer.id,
+		  	:amount      => @amount,
+		  	:description => 'Rails Stripe customer',
+		  	:currency    => 'usd'
+		  	)
 
-	rescue Stripe::CardError => e
-	  flash[:error] = e.message
-	  redirect_to charges_path
+			puts 'DIDNT ERROR!'
+
+		  redirect_to root_path
+
+		rescue Stripe::CardError => e
+			puts 'ERRORED!'
+			flash[:error] = e.message
+			redirect_to charges_path
+		end
 	end
 
+	private
+
+	def charges_params
+		params.require(:charges).permit(:token, :amount)
+	end
 end
