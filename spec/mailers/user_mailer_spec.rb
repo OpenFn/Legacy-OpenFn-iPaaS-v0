e92ae5d7ee910cc7a04442a9a9f133f1b338676b
Rory McKinley <rorymckinley@gmail.com>
2015-07-05 22:11:23 +0200

    Fix mailer spec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,12 +2,13 @@ require "rails_helper"
 
 RSpec.describe UserMailer, :type => :mailer do
   describe "reset_password_email" do
-    let(:mail) { UserMailer.reset_password_email }
+    let(:mail) { UserMailer.reset_password_email(user) }
+    let(:user) { double(email: "to@example.org", reset_password_token: "123ABC", first_name: "John") }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Reset password email")
+      expect(mail.subject).to eq("Your openfn.org password has been reset.")
       expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.from).to eq(["authentication@openfn.org"])
     end
 
     it "renders the body" do
