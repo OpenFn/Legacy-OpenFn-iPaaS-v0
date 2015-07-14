bf6a6590709dc693a1f3bce321d75b771ceab9f2
taylor <downs.taylor@gmail.com>
2015-07-14 15:36:59 +0200

    added back marshalling

--- app/models/submission/record.rb
+++ app/models/submission/record.rb
@@ -4,6 +4,15 @@ class Submission::Record < ActiveRecord::Base
 
   validates_presence_of :mapping_id, :raw_source_payload
 
+  # Marshal data structures into JSON, 		
+  # ideally all integrations return strings.		
+  def raw_source_payload=(payload)		
+    if !payload.is_a?(String) && !payload.nil?		
+      return super(payload.to_json) 		
+    end		
+    super		
+  end
+
   def submitted!
     self.processed_at = Time.now
     self.save!
