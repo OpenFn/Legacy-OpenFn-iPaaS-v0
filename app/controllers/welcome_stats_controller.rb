073bdb951561040f5c71a7bbaedb6b30c90218f9
taylor <downs.taylor@gmail.com>
2015-09-04 12:19:48 -0400

    added myargo stats to welcomestats controller

--- app/controllers/welcome_stats_controller.rb
+++ app/controllers/welcome_stats_controller.rb
@@ -4,8 +4,10 @@ skip_before_filter :require_login
 
   def show
     render json: {
-      #We add 35,605 to submissionCount for KGVK's pre-reset submissions
-      submissionCount: OdkSfLegacy::Submission.where(state: 'success').count + 35605,
+      #KGVK: We add 35,605 to submissionCount for KGVK's "pre-reset" submissions.
+      #MyAgro: Installed OpenFn on their SF instance on 12/23/14.
+      #MyAgro (cont.): We add 96,001 (as of 9/14/15) for their SMS transactions since installing.
+      submissionCount: OdkSfLegacy::Submission.where(state: 'success').count + 35605 + 96001,
       orgCount: User.distinct.count('organisation'),
       productPublicCount: Product.where(enabled: true).count,
       productConnectedCount: Product.where(integrated: true).count
