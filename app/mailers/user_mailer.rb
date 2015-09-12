7a8a58742667556578b89bd0b0ac452ebc925b23
taylor <downs.taylor@gmail.com>
2015-09-12 16:56:22 -0700

    initial sorcery activaion setup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,4 +12,17 @@ class UserMailer < ActionMailer::Base
     mail(:to => user.email,
          :subject => "Your openfn.org password has been reset.")
   end
-end
+
+  def activation_needed_email(user)
+    @user = user
+    @url  = "http://0.0.0.0:3000/users/#{user.activation_token}/activate"
+    mail(:to => user.email,
+         :subject => "Welcome to My Awesome Site")
+  end
+
+  def activation_success_email(user)
+    @user = user
+    @url  = "http://0.0.0.0:3000/login"
+    mail(:to => user.email,
+         :subject => "Your account is now activated")
+  end
